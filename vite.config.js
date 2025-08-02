a1523adf3e170d281d5aedd6d7d0ec36a87b395d
Saravasha <33827803+Saravasha@users.noreply.github.com>
2025-08-03 00:52:05 +0200

    aweawe

--- vite.config.js
+++ vite.config.js
@@ -9,36 +9,35 @@ export default defineConfig({
   "process.env": {
     DOTNET_ASSET_API_URL:
       process.env.VITE_DOTNET_ASSET_API_URL ||
-      "https://admin.__DOMAIN__.com/api/react/asset",
+      "https://admin.__DOMAIN__/api/react/asset",
     DOTNET_ASSET_API_URL_STAGE:
       process.env.VITE_DOTNET_ASSET_API_URL ||
-      "https://admin-staging.__DOMAIN__.com/api/react/asset",
+      "https://admin-staging.__DOMAIN__/api/react/asset",
     DOTNET_ASSET_API_URL_DEV:
       process.env.VITE_DOTNET_ASSET_API_URL ||
       "http://localhost:5000/api/react/asset",
     DOTNET_PAGE_API_URL:
       process.env.VITE_DOTNET_PAGE_API_URL ||
-      "https://admin.__DOMAIN__.com/api/react/page",
+      "https://admin.__DOMAIN__/api/react/page",
     DOTNET_PAGE_API_URL_STAGE:
       process.env.VITE_DOTNET_PAGE_API_URL ||
-      "https://admin-staging.__DOMAIN__.com/api/react/page",
+      "https://admin-staging.__DOMAIN__/api/react/page",
     DOTNET_PAGE_API_URL_DEV:
       process.env.VITE_DOTNET_PAGE_API_URL ||
       "http://localhost:5000/api/react/page",
     DOTNET_COLOR_API_URL:
       process.env.VITE_DOTNET_COLOR_API_URL ||
-      "https://admin.__DOMAIN__.com/api/react/color",
+      "https://admin.__DOMAIN__/api/react/color",
     DOTNET_COLOR_API_URL_STAGE:
       process.env.VITE_DOTNET_COLOR_API_URL ||
-      "https://admin-staging.__DOMAIN__.com/api/react/color",
+      "https://admin-staging.__DOMAIN__/api/react/color",
     DOTNET_COLOR_API_URL_DEV:
       process.env.VITE_DOTNET_COLOR_API_URL ||
       "http://localhost:5000/api/react/color",
     DOTNET_API_URL:
-      process.env.VITE_DOTNET_API_TARGET || "https://admin.__DOMAIN__.com/",
+      process.env.VITE_DOTNET_API_TARGET || "https://admin.__DOMAIN__/",
     DOTNET_API_URL_STAGE:
-      process.env.VITE_DOTNET_API_TARGET ||
-      "https://admin-staging.__DOMAIN__.com/",
+      process.env.VITE_DOTNET_API_TARGET || "https://admin-staging.__DOMAIN__/",
     DOTNET_API_URL_DEV:
       process.env.VITE_DOTNET_API_TARGET || "http://localhost:5000/",
   },
